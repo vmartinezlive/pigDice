bcb7c502ce19919cc1b1797d183997ff0b46a122
maly phongsavanh and victoria martinez <victoria@>
2018-11-01 10:47:19 -0700

    add user interface on js

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,59 @@
 // business logic
-var add = function(a, b) {
-  return a + b;
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-var player1 = new Players ("Victoria");
-var player2 = new Players ("Maly");
 
-function Players(name, score, dice) {
+function Players(name, dice) {
   this.name = name,
-  this.score = [],
-  this.dice = dice,
-  this.totalScore = 0
+  this.totalScore = 0,
+  this.dice = dice
 }
 
-Players.prototype.diceRoll = function(dice) {
-    this.dice = Math.floor(Math.random() * 6) + 1;
-    if (this.dice === 1) {
-      this.score = [];
-    } else if (this.dice >= 2 || this.dice <=6 && this.dice !== 1) {
-    this.score.push(this.dice);
+Players.prototype.diceRoll = function() {
+  this.dice = rollDice();
+  if (this.dice === 1) {
+    return;
+  } else if (this.dice >= 2 || this.dice <=6 && this.dice !== 1) {
+    return this.totalScore += this.dice;
   }
-  return this.totalScore = this.score.reduce(add, 0);
+
 }
 
 Players.prototype.holding = function() {
-  if (this.dice >=2 || this.dice <=6 && this.dice !== 1) {
-    this.score.push(this.dice);
-  }
-  return this.totalScore = this.score.reduce(add, 0);
-  this.score = [];
+  return;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-//Prints dice roll to the page
-function printNumber(number) {
-  var placeholder = document.getElementById('placeholder');
-  placeholder.innerHTML = number;
-}
 // // useriterface logic
 $(document).ready(function(){
-  var rollButton = document.getElementById('roll-button');
-  var holdButton = document.getElementById('hold-button');
+  $(".player-select").submit(function(event) {
+    event.preventDefault();
 
+    var player1 = new Players ($("#player1").val());
+    var player2 = new Players ($("#player2").val());
 
-  rollButton.onclick = function() {
-    var result = player1.diceRoll();
-    printNumber(result);
-    console.log(player1);
-    };
+    $("#player1Name").text(player1.name);
+    $("#player2Name").text(player2.name);
+    $("#players-display").show();
+    $(".player-select").hide();
 
-  holdButton.onclick = function() {
-    var result = player1.holding();
-    printNumber(result);
-  };
+    $("#player1-button").click(function(){
+      player1.diceRoll();
+      $("#placeholder").text(player1.dice);
+      $("#hold-button").click(function(){
+        player1.holding();
+        $("#player1Score").text(player1.totalScore);
+      })
+    })
 
-
+    $("#player2-button").click(function() {
+      player2.diceRoll();
+      $("#placeholder").text(player2.dice);
+      $("#2hold-button").click(function() {
+        player2.holding();
+        $("#player2Score").text(player2.totalScore);
+      })
+    })
+  })
 });
